adbcc41752f51c4f0e742d0ac75a20323bcb5e54
Dilpreet Kaur <dkaur175@myseneca.ca>
2026-07-17 08:52:06 -0400

    Complete Assignment 2 milestone 1

--- assignment2.py
+++ assignment2.py
@@ -3,7 +3,7 @@
 '''
 OPS445 Assignment 2 - Winter 2023
 Program: assignment2.py 
-Author: "Student Name"
+Author: "Dilpreet kaur"
 The python code in this file is original work written by
 "Student Name". No code in this file is copied from any other source 
 except those provided by the course instructor, including any person, 
@@ -12,9 +12,9 @@ with anyone or anything except for submission for grading.
 I understand that the Academic Honesty Policy will be enforced and 
 violators will be reported and appropriate action will be taken.
 
-Description: <Enter your documentation here>
+Description: Displays total and available system memory using a bar graph.
 
-Date: 
+Date:17 July 2026 
 
 '''
 
@@ -32,17 +32,23 @@ def parse_command_args() -> object:
 
 def percent_to_graph(percent: float, length: int=20) -> str:
     "turns a percent 0.0 - 1.0 into a bar graph"
-    pass
+    hashes = round(percent * length)
+    spaces = length - hashes
+    return "#" * hashes + " " * spaces
 
 def get_sys_mem() -> int:
     "return total system memory (used or available) in kB"
-    # open the meminfo file to do this!
-    pass
+    with open("/proc/meminfo", "r") as file:
+        for line in file:
+            if line.startswith("MemTotal:"):
+                return int(line.split()[1])
 
 def get_avail_mem() -> int:
     "return total memory that is currently available"
-    # open the meminfo file to do this!
-    pass
+    with open("/proc/meminfo", "r") as file:
+        for line in file:
+            if line.startswith("MemAvailable:"):
+                return int(line.split()[1])
 
 def pids_of_prog(app_name: str) -> list:
     "given an app name, return all pids associated with app"
